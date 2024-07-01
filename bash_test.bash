0e6b4dfa2e6494ac59a10215b3ac9d7d98b9dbce
Miguel Fernandez Cortizas <miguel.fernandez.cortizas@upm.es>
2024-07-01 09:53:24 +0000

    [rm] remove test file

--- bash_test.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This is a comment
-#mavlink UDP 14581, remote port: 14541)
-
-# ros2 launch mavros px4.launch  namespace:=drone0 fcu_url:=udp://:14540@127.0.0.1:14557 
-# ros2 launch mavros px4.launch  namespace:=drone0 fcu_url:=udp://:14ijkj541@127.0.0.1:14581
-ros2 launch mavros px4.launch  namespace:=drone0 fcu_url:="udp://:14540@127.0.0.1:14557"
